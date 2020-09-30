062454673e9a28cb262977df9368eab3431b9beb
Bernd Mumme <b.mumme@stud.uni-heidelberg.de>
2020-10-01 00:18:19 +0200

    adding files

---
+++ automate_insta_bot.ps1
@@ -0,0 +1,5 @@
+.\install_choco.ps1;
+.\install_firefox.ps1;
+.\install_python.ps1;
+.\install_gecko.ps1;
+.\create_venv.ps1;
---
+++ create_venv.ps1
@@ -0,0 +1,4 @@
+python -m venv C:\envs\insta;
+cd C:\envs\insta\Scripts;
+activate;
+pip install selenium
